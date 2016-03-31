fa382f647023c84ae5810345e8ca61adfb12c567
Juraj Kardos <juraj.kardos@usi.ch>
2016-03-31 16:44:17 +0200

    Script for running the problem, it sets the params in the ipopt.opt file

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This scipt sets proper options in ipopt.opt required by SchurSolver.
+# Particularly the dimension of the grid N and the number of stochastic scenarios NS.
+# These parameter has to match with runtime parameters of the solve_problems
+# $ mpirun -n NP ./solve_problem N NS, where N NS has to be same in the ipopt.opt
+
+# check command line arguments
+if [ $# -ne 3 ]; then
+    echo 'Usage: $./run.sh NP N NS'
+    exit
+fi
+
+NP=$1 # number of processes
+N=$2  # dimension of the grid
+NS=$3 # number of scenarios
+
+if [ -e ipopt.opt ]; then
+    sed -i "s/^problem_dimension\ [0-9]*[[:space:]]*$/problem_dimension\ $N/g" ipopt.opt
+    sed -i "s/^problem_scenarios\ [0-9]*[[:space:]]*$/problem_scenarios\ $NS/g" ipopt.opt
+else
+    echo "problem_dimension $N" > ipopt.opt
+    echo "problem_scenarios $NS" >> ipopt.opt
+fi
+
+mpirun -n $NP ./solve_problem $N $NS
