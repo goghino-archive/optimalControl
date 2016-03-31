418837033bd75223282d61fcff5e87a3f948264f
Juraj Kardos <juraj.kardos@usi.ch>
2016-03-31 14:47:38 +0200

    Removed unnecessary code from PardisoInterface

--- src/Algorithm/LinearSolvers/IpPardisoSolverInterface.hpp
+++ src/Algorithm/LinearSolvers/IpPardisoSolverInterface.hpp
@@ -123,16 +123,6 @@ namespace Ipopt
     Index NS_;
     //@}
 
-#ifdef PARDISO_MATCHING_PREPROCESS
-    /** Array for storing the values of a second matrix that has been already reordered. */
-    ipfint* ia2;
-    ipfint* ja2;
-    double* a2_;
-    ipfint* perm2;
-    double* scale2;
-
-#endif
-
     /** @name Information about most recent factorization/solve */
     //@{
     /** Number of negative eigenvalues */
@@ -141,34 +131,6 @@ namespace Ipopt
 
     /** @name Solver specific options */
     //@{
-    /** Type for mathcing strategies */
-    enum PardisoMatchingStrategy
-    {
-      COMPLETE,
-      COMPLETE2x2,
-      CONSTRAINT
-    };
-    /** Option that controls the matching strategy. */
-    PardisoMatchingStrategy match_strat_;
-    /** Flag indicating if symbolic factorization has already been
-     *  performed. */
-    bool have_symbolic_factorization_;
-    /** Flag indicating whether the symbolic factorization should only
-     *  be done after perturbed elements, if the inertia was wrong */
-    bool pardiso_redo_symbolic_fact_only_if_inertia_wrong_;
-    /** Flag indicating whether repeated perturbed elements even after
-     *  a new symbolic factorization should be interpreted as a
-     *  singular matrix */
-    bool pardiso_repeated_perturbation_means_singular_;
-    /** Flag indicating if the interia is always assumed to be
-      *  correct. */
-    bool skip_inertia_check_;
-    /** Flag indicating whether we are using the iterative solver in
-      * Pardiso. */
-    bool pardiso_iterative_;
-    /** Maximal number of decreases of drop tolerance during one solve. */
-    Index pardiso_max_droptol_corrections_;
-    //@}
 
     /** @name Initialization flags */
     //@{
@@ -177,51 +139,6 @@ namespace Ipopt
     bool initialized_;
     //@}
 
-    /** @name Solver specific information */
-    //@{
-    /** Internal data address pointers. */
-    void** PT_;
-    /** Maximal number of factors with identical nonzero
-     *  structure. Here, we only store one factorization. Is always 1.*/
-    ipfint MAXFCT_;
-    /** Actual matrix for the solution phase. Is always 1.*/
-    ipfint MNUM_;
-    /** Matrix type; real and symmetric indefinite.  Is always -2.*/
-    ipfint MTYPE_;
-    /** Parameter and info array for Pardiso. */
-    ipfint* IPARM_;
-    /** Parameter and info array for Pardiso. */
-    double* DPARM_;
-    /** Message level. */
-    ipfint MSGLVL_;
-    //@}
-
-    /**@name Some counters for debugging */
-    //@{
-    Index debug_last_iter_;
-    Index debug_cnt_;
-    //@}
-
-    /** @name Internal functions */
-    //@{
-    /** Call Pardiso to do the analysis phase.
-     */
-    ESymSolverStatus SymbolicFactorization(const Index* ia,
-                                           const Index* ja);
-
-    /** Call Pardiso to factorize the Matrix.
-     */
-    ESymSolverStatus Factorization(const Index* ia,
-                                   const Index* ja,
-                                   bool check_NegEVals,
-                                   Index numberOfNegEVals);
-
-    /** Call Pardiso to do the Solve.
-     */
-    ESymSolverStatus Solve(const Index* ia,
-                           const Index* ja,
-                           Index nrhs,
-                           double *rhs_vals);
     //@}
   };
 
