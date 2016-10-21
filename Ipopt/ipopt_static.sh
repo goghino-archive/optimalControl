2fd3fbae086d04aaad3307da3af1cd6c37974572
Juraj Kardos <juraj.kardos@usi.ch>
2016-10-21 14:37:38 +0200

    adjusting static configure to ICSMASTER

--- Ipopt/ipopt_static.sh
+++ Ipopt/ipopt_static.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#MKLROOT=/opt/intel/compilers_and_libraries_2017/linux/mkl #archimedes
+MKLROOT=/apps/intel/17.0.0/mkl #ICS
 PARDISO_DIR=/home/kardos/lib/pardiso
-#MKLROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mkl
-MKLROOT=/opt/intel/compilers_and_libraries_2017/linux/mkl
+MATLAB=/apps/matlab/R2016a
 
 ../configure ADD_CXXFLAGS="-fPIC -fopenmp -m64" F77=gfortran CC=gcc CXX=g++ \
     --enable-static --enable-inexact-solver --disable-shared --with-pic \
     --enable-matlab-static --disable-dependency-linking --without-ma27 \
      --with-blas-lib="-Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_lp64.a ${MKLROOT}/lib/intel64/libmkl_core.a ${MKLROOT}/lib/intel64/libmkl_gnu_thread.a -Wl,--end-group -ldl -lpthread -lm"  \
     --with-lapack-lib="${MKLROOT}/lib/intel64/libmkl_lapack95_lp64.a" \
-    --with-matlab-home=/opt/MATLAB/R2014b \
+    --with-matlab-home=${MATLAB} \
     --with-pardiso="-Wl,--start-group ${PARDISO_DIR}/libpardiso.a  ${PARDISO_DIR}/libpils_pardiso.a ${PARDISO_DIR}/libmetis41_pardiso.a  ${PARDISO_DIR}/libmetis41-P_pardiso.a -Wl,--end-group  -ldl -lpthread -lm -lgomp -lgfortran" 
