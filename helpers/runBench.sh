b16c5e940ce8f01a180c7e0e0077ec9ae3cd8b3f
Juraj Kardos <juraj.kardos@usi.ch>
2016-10-26 16:01:52 +0200

    updated runBench.sh

--- helpers/runBench.sh
+++ helpers/runBench.sh
@@ -3,12 +3,12 @@
 # This scipt sets proper options in ipopt.opt required by SchurSolver.
 # Particularly the dimension of the grid N and the number of stochastic scenarios NS.
 # These parameter has to match with runtime parameters of the solve_problems
-# $ mpirun -n NP ./solve_problem N NS, where N NS has to be same in the ipopt.opt
-# Example:
-# ./run.sh 2 10 2 schur     run 2 MPI processes using Schur solver (N>1 always)
-# ./run.sh 1 10 2 pardiso   run !!single!! process with Pardiso (N=1 always)
 
 MKLROOT=/apps/intel/17.0.0/mkl
+SHCUR_DIR=~/PowerGrid
+
+#location of the executable
+PREFIX=/home/kardos/optimalControl/build/Ipopt/examples/ScalableProblems
 
 # check command line arguments
 #if [ $# -ne 4 ]; then
@@ -21,10 +21,7 @@ N=300  # dimension of the grid
 NS=128 # number of scenarios
 SOLVER=schur #linear solver
 
-#location of the executable
-PREFIX=/home/kardos/optimalControl/build/Ipopt/examples/ScalableProblems
-
-#set ipopt.opt options
+#set ipopt.opt options N NS and solver
 if [ -e ${PREFIX}/ipopt.opt ]; then
     sed -i "s/^problem_dimension\ [0-9]*[[:space:]]*$/problem_dimension\ $N/g" ${PREFIX}/ipopt.opt
     sed -i "s/^problem_scenarios\ [0-9]*[[:space:]]*$/problem_scenarios\ $NS/g" ${PREFIX}/ipopt.opt
@@ -35,7 +32,7 @@ else
     echo "linear_solver $SOLVER" >> ${PREFIX}/ipopt.opt
 fi
 
-for comp_nodes in 1 2 16 32; do
+for comp_nodes in 1 2 4 8 16; do
 
 #master process is not computing, only distributes work
 tot_nodes=$((comp_nodes + 1))
@@ -46,13 +43,13 @@ sbatch <<-_EOF
 #SBATCH --job-name=OC_${comp_nodes}_${N}_${NS}
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=${tot_nodes}
-#SBATCH --time=02:30:00
+#SBATCH --time=04:30:00
 #SBATCH --output=job_${comp_nodes}_${N}_${NS}.out
 
 export OMP_NUM_THREADS=8
 export PARDISOLICMESSAGE=1
 
-LD_LIBRARY_PATH=~/PowerGrid/lib:${MKLROOT}/lib/intel64:${LD_LIBRARY_PATH} \
+LD_LIBRARY_PATH=${SCHUR_DIR}/lib:${MKLROOT}/lib/intel64:${LD_LIBRARY_PATH} \
 mpirun -np ${tot_nodes} -N 1 ${PREFIX}/solve_problem $N $NS
 
 _EOF
